5b5d6116b97371ca9765df19dd4739b5c103b374
Matheus <email>
2025-04-11 17:12:38 -0300

    feat(front): criado componente blocked reutilizável e refatorado estrutura da página
        Implementado o componente blockedComponent para encapsular a mensagem de acesso negado.
    
        Criado o arquivo de estilo blockedComponent.css com visual específico e isolado para a interface de bloqueio.
    
        Refatorado blocked.html para utilizar estrutura baseada em componentes reutilizáveis, adicionando header, footer e main para padronização.
    
        Substituído conteúdo direto no HTML por inserção dinâmica via JavaScript, facilitando manutenção e escalabilidade da aplicação.

---
+++ nginx-clinic/html/scripts/components/authentication/blockedComponent.js
@@ -0,0 +1,6 @@
+// scripts/components/authetication/blockedComponent.js
+
+document.querySelector(".blocked-component").innerHTML = `
+    <h1>Você não tempermisão para acessar a página.</h1>
+    <button id="logout">Voltar</button>
+`;
