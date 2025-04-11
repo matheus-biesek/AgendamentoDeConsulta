a6b449bcd5816cc3beb62f58cb58ed16b84fc7c8
Matheus <email>
2025-04-11 00:33:02 -0300

    Projeto autentificação e autorização. Criação do endpoint REST base para requisições da aplicação.

---
+++ java_ee_auth/src/main/java/com/code/java_ee_auth/RestApplication.java
@@ -0,0 +1,7 @@
+package com.code.java_ee_auth;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/rest-auth")
+public class RestApplication extends Application {}
