52f0913cc4bd3a3de642dcbe6997218a1c9cb12c
brufabri <162648907+brufabri@users.noreply.github.com>
2025-04-22 16:20:54 -0300

    html para alterar senha

---
+++ nginx-clinic/html/scripts/components/authentication/changePasswordComponent.js
@@ -0,0 +1,17 @@
+document.querySelector(".change-password-component").innerHTML = `
+        <div class="change-password">
+            <h1>Trocar Senha</h1>
+            <form id="change-password-form">
+                <label for="last-password">Senha Atual:</label>
+                <input type="password" id="last-password" required>
+                
+                <label for="new-password">Nova Senha:</label>
+                <input type="password" id="new-password" required>
+                
+                <label for="confirm-password">Confirme a Nova Senha:</label>
+                <input type="password" id="confirm-password" required>
+                
+                <button type="button" id="change-password-button">Trocar Senha</button>
+            </form>
+        </div>
+    `;
