c8b6edf769c325f9edcdf6858d572c24bbc6486e
Matheus <email>
2025-04-29 19:55:44 -0300

    chore(auth-service): Atualização da classe JWTUtils para AccessJWTService e substituição de username por UUID
    O que foi feito?
    - A classe `JWTUtils` foi refatorada para `AccessJWTService`, agora utilizando `UUID` para identificar o usuário em vez de `username` (String).
    - A variável de expiração do token foi alterada para uma constante de 15 minutos (`REFRESH_TOKEN_EXPIRATION_MS`).
    - A classe agora está anotada com `@ApplicationScoped` para ser gerida pelo CDI, permitindo injeção de dependências no futuro.
    - O método de validação de role foi mantido mas será removido futuramente.
    
    Por que foi feito?
    - A alteração de `username` para `UUID` melhora a segurança e rastreabilidade, alinhando-se com boas práticas para identificar usuários de maneira única e segura.
    - A refatoração para `AccessJWTService` facilita a futura injeção de dependências e torna o serviço mais coeso, adequado para ser usado como um serviço acessível em outras partes da aplicação.
    - O novo tempo de expiração de 15 minutos para tokens foi configurado para um controle mais adequado da sessão.
    - A mudança visa também facilitar a escalabilidade da aplicação, uma vez que o uso de UUID é mais flexível e seguro em sistemas grandes e distribuídos.
    
    Como foi feito?
    - A classe `JWTUtils` foi renomeada e movida para o pacote `application.service.security`, agora chamada `AccessJWTService`.
    - O método `generateToken` foi modificado para aceitar `UUID` no lugar de `String` para o identificador do usuário.
    - A chave de expiração do token foi configurada como `15 minutos` com a constante `REFRESH_TOKEN_EXPIRATION_MS`.
    - A validação de role foi mantida, mas um comentário foi adicionado indicando que será removido futuramente, já que a aplicação não precisa mais dessa validação.

--- java_ee_auth/src/main/java/com/code/java_ee_auth/application/service/security/AccessJWTService.java
+++ java_ee_auth/src/main/java/com/code/java_ee_auth/application/service/security/AccessJWTService.java
@@ -1,4 +1,4 @@
-package com.code.java_ee_auth.adapters.security.jwt;
+package com.code.java_ee_auth.application.service.security;
 
 import com.code.java_ee_auth.domain.enuns.UserRole;
 import io.jsonwebtoken.Claims;
@@ -8,16 +8,23 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
+import java.util.UUID;
 
-public class JWTUtils {
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class AccessJWTService {
+
+    // Adicione a chave secreta aqui
     private static final String SECRET_KEY = "ewoCu39mGULU1oFgkIoy6Z2OEjvXd4Y1jzL/p60Xu1I=";
+    private static final long REFRESH_TOKEN_EXPIRATION_MS = 15 * 60 * 1000; // 1 minutos em milissegundos
 
-    private static Key getSigningKey() {
+    private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public static Claims parseToken(String token) {
+    public Claims parseToken(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
@@ -25,18 +32,19 @@ public class JWTUtils {
                 .getBody();
     }
 
-    public static String generateToken(String username, String role, String csrfToken) {
+    public String generateToken(UUID userId, String role, String csrfToken) {
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(userId.toString())
                 .claim("role", role)
                 .claim("csrf", csrfToken)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + ((60 * 60) * 60000))) // 6 horas
+                .setExpiration(new Date((long) (System.currentTimeMillis() + REFRESH_TOKEN_EXPIRATION_MS))) 
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public static boolean validateTokenRole(String token, UserRole expectedRole) {
+    // Futuramente apaguar este método pois está aplicação não precissará de validar o token
+    public boolean validateTokenRole(String token, UserRole expectedRole) {
         try {
             Claims claims = parseToken(token);
             String roleString = claims.get("role", String.class);  // Obtém a role como String
