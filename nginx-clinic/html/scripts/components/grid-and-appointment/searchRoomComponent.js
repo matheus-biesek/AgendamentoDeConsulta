d61ee8b13fbd3a074d5703aa837d46d52d520443
brufabri <162648907+brufabri@users.noreply.github.com>
2025-05-07 15:30:24 -0300

    Create searchRoomComponent.js

---
+++ nginx-clinic/html/scripts/components/grid-and-appointment/searchRoomComponent.js
@@ -0,0 +1,21 @@
+document.querySelector(".search-all-room-component").innerHTML = `
+    <div class="search-room-container">
+        <h2>Salas</h2>
+        <div class="search-room-grid">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Número</th>
+                        <th>Função</th>
+                    </tr>
+                </thead>
+                <tbody id="room-list">
+                    <tr>
+                        <td>101</td>
+                        <td>Exame</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+`;
