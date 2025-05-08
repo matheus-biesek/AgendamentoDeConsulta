25d6e211fa0c219106ee209ae217b04c30ecc0cf
Matheus <email>
2025-05-07 21:33:31 -0300

    chore(lib): Ajuste no target das bibliotecas

--- rabbitmq-lib/src/main/java/com/rabbitmq/lib/example/SimpleProducer.java
+++ rabbitmq-lib/src/main/java/com/rabbitmq/lib/example/SimpleProducer.java
@@ -2,10 +2,8 @@ package com.rabbitmq.lib.example;
 
 import com.rabbitmq.lib.AbstractRabbitMQProducer;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Named;
 
 @ApplicationScoped
-@Named("simpleProducer")
 public class SimpleProducer extends AbstractRabbitMQProducer {
 
     @Override
