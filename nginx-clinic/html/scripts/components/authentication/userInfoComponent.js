23601c743ee3c9b005dc8537148a0d1a4e485e3f
Matheus <email>
2025-05-08 17:58:13 -0300

    feat(front): Criação do componente auxiliar para ver as informações de usuario na hora do login

---
+++ nginx-clinic/html/scripts/components/authentication/userInfoComponent.js
@@ -0,0 +1,48 @@
+document.querySelector(".user-info-component").innerHTML = `
+    <div class="line">
+        <div class="user-info">
+            <p>ROLE: ADMIN</p>
+            <p>CPF: 12345678900</p>
+            <p>SENHA: 1234</p>
+        </div>
+
+        <div class="user-info">
+            <p>ROLE: ADMIN</p>
+            <p>CPF: 12345678901</p>
+            <p>SENHA: 1234</p>
+        </div>
+
+        <div class="user-info">
+            <p>ROLE: SECRETARY</p>
+            <p>CPF: 12345678902</p>
+            <p>SENHA: 1234</p>
+        </div>
+
+        <div class="user-info">
+            <p>ROLE: NURSE</p>
+            <p>CPF: 12345678903</p>
+            <p>SENHA: 1234</p>
+        </div>
+    </div>
+
+    <div class="line">
+
+        <div class="user-info">
+            <p>ROLE: PATIENT</p>
+            <p>CPF: 12345678904</p>
+            <p>SENHA: 1234</p>
+        </div>
+
+        <div class="user-info">
+            <p>ROLE: TECHNICIAN</p>
+            <p>CPF: 12345678905</p>
+            <p>SENHA: 1234</p>
+        </div>
+
+        <div class="user-info">
+            <p>ROLE: DOCTOR</p>
+            <p>CPF: 12345678906</p>
+            <p>SENHA: 1234</p>
+        </div>    
+    </div>
+`;
