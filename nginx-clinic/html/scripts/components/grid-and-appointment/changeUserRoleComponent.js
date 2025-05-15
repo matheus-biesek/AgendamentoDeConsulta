ca3fad852cf7a5cd4a2a50c49882bcf9b37d48e5
brufabri <162648907+brufabri@users.noreply.github.com>
2025-05-14 23:33:15 -0300

    Update changeUserRoleComponent.js

--- nginx-clinic/html/scripts/components/grid-and-appointment/changeUserRoleComponent.js
+++ nginx-clinic/html/scripts/components/grid-and-appointment/changeUserRoleComponent.js
@@ -7,22 +7,24 @@ document.querySelector(".change-user-role-component").innerHTML = `
                 <input type="text" id="cpf" placeholder="Digite o CPF do usuário" required>
             </div>
             <div class="form-group">
-                <label>Role:</label>
                 <div class="role-options">
                     <input type="radio" id="admin-change" name="role-change" value="Admin" required>
                     <label for="admin-change">Admin</label>
 
-                    <input type="radio" id="doutor-change" name="role-change" value="Doutor">
-                    <label for="doutor-change">Doutor</label>
+                    <input type="radio" id="doctor-change" name="role-change" value="Doutor">
+                    <label for="doctor-change">Doutor</label>
 
-                    <input type="radio" id="paciente-change" name="role-change" value="Paciente">
-                    <label for="paciente-change">Paciente</label>
+                    <input type="radio" id="patient-change" name="role-change" value="Paciente">
+                    <label for="pactient-change">Paciente</label>
 
-                    <input type="radio" id="tecnico-change" name="role-change" value="Técnico">
-                    <label for="tecnico-change">Técnico</label>
+                    <input type="radio" id="technician-change" name="role-change" value="Técnico">
+                    <label for="technician-change">Técnico</label>
 
-                    <input type="radio" id="secretario-change" name="role-change" value="Secretario">
-                    <label for="secretario-change">Secretario</label>
+                    <input type="radio" id="secretary-change" name="role-change" value="Secretario">
+                    <label for="secretary-change">Secretario</label>
+
+                    <input type="radio" id="nurse-change" name="role-change" value="Enfermeira">
+                    <label for="nurse-change">Enfermeira</label>
                 </div>
             </div>
             <button type="submit" id="update-role-button">Alterar</button>
