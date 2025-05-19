b64f03bd7375e39471e42064dbe9b33a662d4fb8
brufabri <162648907+brufabri@users.noreply.github.com>
2025-05-19 15:41:24 -0300

    Update createScheduleComponent.js

--- nginx-clinic/html/scripts/components/grid-and-appointment/createScheduleComponent.js
+++ nginx-clinic/html/scripts/components/grid-and-appointment/createScheduleComponent.js
@@ -4,19 +4,13 @@ document.querySelector(".create-schedule-component").innerHTML = `
             <div class="form-group">
                 <label for="worker-id">ID Funcionário:</label>
                 <input type="number" id="worker-id" min="1" placeholder="Digite o ID do funcionário" required>
-            </div>
-
-            <div class="form-group">
+  
                 <label for="room-number">Sala:</label>
                 <input type="number" id="room-number" min="1" placeholder="Digite o número da sala" required>
-            </div>
 
-            <div class="form-group">
                 <label for="schedule-date">Data:</label>
                 <input type="date" id="schedule-date" required>
-            </div>
 
-            <div class="form-group">
                 <label for="schedule-time">Hora:</label>
                 <input type="time" id="schedule-time" step="1800"required>
             </div>
