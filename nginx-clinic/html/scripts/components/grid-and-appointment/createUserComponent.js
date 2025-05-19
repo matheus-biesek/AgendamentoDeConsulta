0793044cdd51be0abe949ec86a2ceec23b047b8c
brufabri <162648907+brufabri@users.noreply.github.com>
2025-05-19 15:41:30 -0300

    Update createUserComponent.js

--- nginx-clinic/html/scripts/components/grid-and-appointment/createUserComponent.js
+++ nginx-clinic/html/scripts/components/grid-and-appointment/createUserComponent.js
@@ -4,19 +4,20 @@ document.querySelector(".create-user-component").innerHTML = `
             <div class="form-group">
                 <label for="name-create">Nome de Usuário:</label>
                 <input type="text" id="name-create" placeholder="Digite o nome de usuário" required>
-            </div>
-            <div class="form-group">
+
                 <label for="cpf-create">CPF:</label>
                 <input type="text" id="cpf-create" placeholder="Digite o CPF do usuário" required>
-            </div>
-            <div class="form-group">
+
+                <label for="cpf-create">Email:</label>
+                <input type="text" id="email-create" placeholder="Digite o email do usuário" required>
+
                 <label for="password-create">Senha:</label>
                 <input type="password" id="password-create" placeholder="Digite a senha" required>
-            </div>
-            <div class="form-group">
+
                 <label for="confirm-password-create">Confirma Senha:</label>
                 <input type="password" id="confirm-password-create" placeholder="Confirme a senha" required>
             </div>
+
             <button type="submit" id="create-user-button">Criar</button>
         </form>
 `;
